9568311034df533ab96d167c6b6a4f5fdb3923de
Simon Mikkelsen <simonmikkelsen@users.noreply.github.com>
2018-10-20 16:50:06 +0200

    Ignores done or removed folders.

--- find-mapillary-sequences.sh
+++ find-mapillary-sequences.sh
@@ -6,5 +6,5 @@ else
   folder="."
 fi
 
-find "$folder" -type d -name '[0-9]' -or -name '[0-9][0-9]' -or -name '[0-9][0-9][0-9]' | sort -n
+find "$folder" -type d -name '[0-9]' -or -name '[0-9][0-9]' -or -name '[0-9][0-9][0-9]' | egrep -v 'done|removed'  | sort -n
 
