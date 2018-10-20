7514b09ab73f25a1a9ae453d2fcdd2207887ebd5
Simon Mikkelsen <simonmikkelsen@users.noreply.github.com>
2018-10-20 16:52:09 +0200

    Added dry run.

--- upload_unfinished_seqnences.sh
+++ upload_unfinished_seqnences.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-echo "Potential uploads:"
-find-mapillary-sequences.sh
+dryrun=no
+if [  "$1" = "-n" -o "$1" = "--dry-run" ]
+then
+  dryrun=yes
+fi
+
+if [ $dryrun = no ]; then
+  echo "Potential uploads:"
+  find-mapillary-sequences.sh
+fi
 
 find-mapillary-sequences.sh | while read d
 do
@@ -13,10 +21,18 @@ do
   then
     upload=yes
   fi
+  if [ "`find "$d/" -iname '*.jpg' | wc -l`" = "0" ]
+  then
+    upload=no
+  fi
 
   if [ $upload = yes ]
   then
-    mapillary_upload.sh "$d"
+    if [ $dryrun = no ]; then
+      mapillary_upload.sh "$d"
+    else
+      echo "$d"
+    fi
   fi
 done
 
